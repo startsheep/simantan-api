9d89d7b1550acc9c8fc38d4f5175461eec61e083
hakim-asrori <saneglos005@gmail.com>
2022-11-07 19:53:22 +0700

    update filter in comment

---
+++ app/Http/Searches/Filters/Comment/SearchByUser.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Http\Searches\Filters\Comment;
+
+use Closure;
+use Illuminate\Database\Eloquent\Builder;
+use App\Http\Searches\Contracts\FilterContract;
+
+class SearchByUser implements FilterContract
+{
+	/** @var string|null */
+	protected $searchByUser;
+
+	/**
+	 * @param string|null $searchByUser
+	 * @return void
+	 */
+	public function __construct($searchByUser)
+	{
+		$this->searchByUser = $searchByUser;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function handle(Builder $query, Closure $next)
+	{
+		if (!$this->keyword()) {
+			return $next($query);
+		}
+
+        $query->where(function($query) {
+            $query->whereHas('user', function($user) {
+                $user->where('id', $this->searchByUser);
+            });
+        });
+
+		return $next($query);
+	}
+
+	/**
+	 * Get searchByUser keyword.
+	 *
+	 * @return mixed
+	 */
+	protected function keyword()
+	{
+		if ($this->searchByUser) {
+			return $this->searchByUser;
+	    }
+
+		$this->searchByUser = request('user', null);
+
+		return request('user');
+	}
+}
